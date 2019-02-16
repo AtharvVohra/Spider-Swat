bc9dcbae92403dc96c1c043328b7645969fb4e61
TheArcticGiraffe <32856009+TheArcticGiraffe@users.noreply.github.com>
2019-02-16 16:31:36 -0700

    Fixed random movement

--- Scripts/Spider_Movement.gd
+++ Scripts/Spider_Movement.gd
@@ -9,9 +9,9 @@ var velocity = Vector2(1,1)
 func _ready():
 	# Called when the node is added to the scene for the first time.
 	# Initialization here
-	random_vlocity = randi()%50+51
-	velocity.x = random_vlocity
-	velocity.y = random_vlocity
+	var random_velocity = randi()%50+51
+	velocity.x = random_velocity
+	velocity.y = random_velocity
 
 func _process(delta):
 #	# Called every frame. Delta is time since last frame.
