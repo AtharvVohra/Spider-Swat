6749b6345509b03c2173c3967ea84023a9129bae
Atharv Vohra <atharv@ualberta.ca>
2019-02-16 17:53:15 -0700

    mouse cursor hidden

--- Scripts/World.gd
+++ Scripts/World.gd
@@ -7,7 +7,7 @@ extends Node2D
 func _ready():
 	# Called when the node is added to the scene for the first time.
 	# Initialization here
-	pass
+	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
 
 #func _process(delta):
 #	# Called every frame. Delta is time since last frame.
