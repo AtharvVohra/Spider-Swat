4aeca79639f51023de54b14ad34fd96518b93e74
TheArcticGiraffe <32856009+TheArcticGiraffe@users.noreply.github.com>
2019-02-17 12:11:47 -0700

    Properly added spider spawning randomly around edges of viewport.

--- Scripts/Spider_Movement.gd
+++ Scripts/Spider_Movement.gd
@@ -12,15 +12,36 @@ func _ready():
 	var world_rect = get_viewport_rect()
 	randomize()
 	
+	var side = randi()%4 + 1  #Deciding which side the spider will spawn on
+	
+	match side:
+		1:
+			position.x = world_rect.position.x + randi()%int(world_rect.end.x - world_rect.position.x) + 1
+			position.y = world_rect.position.y
+			pass
+		2:
+			position.x = world_rect.end.x
+			position.y = world_rect.position.y + randi()%int(world_rect.end.y - world_rect.position.y) + 1
+			pass
+		3:
+			position.x = world_rect.position.x + randi()%int(world_rect.end.x - world_rect.position.x) + 1
+			position.y = world_rect.end.y
+			pass
+		4:
+			position.x = world_rect.position.x
+			position.y = world_rect.position.y + randi()%int(world_rect.end.y - world_rect.position.y) + 1
+			pass
+		
+	
 	var random_x = randi()%int(world_rect.end.x - world_rect.position.x) + 1
 	var random_y = randi()%int(world_rect.end.y - world_rect.position.y) + 1
 	
-	#position.x = 1024#randi()%int(world_rect.end.x - world_rect.position.x) + 1
-	#position.y = 600#randi()%int(world_rect.end.y - world_rect.position.y) + 1
-	#velocity = Vector2(-50,-50)
+	velocity = Vector2(0,0)
 	randomize()
-	#velocity.x = randi()%200-100
-	#velocity.y = randi()%200-100 
+	
+	#CREATE RANDOMIZED VECTOR
+	
+	
 	pass
 	
 func _process(delta):
