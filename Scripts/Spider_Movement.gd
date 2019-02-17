85c3efdbaee76854a795a0db47ba1f6cc2bf4df2
TheArcticGiraffe <32856009+TheArcticGiraffe@users.noreply.github.com>
2019-02-16 18:34:42 -0700

    Messed around with move_and_collide
    Didn't work

--- Scripts/Spider_Movement.gd
+++ Scripts/Spider_Movement.gd
@@ -11,24 +11,19 @@ func _ready():
 	# Initialization here
 	var world_rect = get_viewport_rect()
 	randomize()
-	print(world_rect.end.x)
-	print(world_rect.end.y)
-	print(world_rect.position.x)
-	print(world_rect.position.y)
+	
 	
 	var random_x = randi()%int(world_rect.end.x - world_rect.position.x) + 1
 	var random_y = randi()%int(world_rect.end.y - world_rect.position.y) + 1
-	
-	print(random_x)
-	print(random_y)
+
 	
 	
-	position.x = 1024#randi()%int(world_rect.end.x - world_rect.position.x) + 1
-	position.y = 600#randi()%int(world_rect.end.y - world_rect.position.y) + 1
-	velocity = Vector2(0,0)
-	#randomize()
-	#velocity.x = randi()%200-100
-	#velocity.y = randi()%200-100 
+	#position.x = 1024#randi()%int(world_rect.end.x - world_rect.position.x) + 1
+	#position.y = 600#randi()%int(world_rect.end.y - world_rect.position.y) + 1
+	#velocity = Vector2(-50,-50)
+	randomize()
+	velocity.x = randi()%200-100
+	velocity.y = randi()%200-100 
 	pass
 	
 func _process(delta):
